67aa04dcf9a6ea5bbccb0469996b4c9d9a9d5f42
bryskiewiczr <bryskiewiczr@protonmail.com>
2025-01-08 14:51:06 +0100

    swap between idle and running animations

--- main.cpp
+++ main.cpp
@@ -19,7 +19,9 @@ int main() {
     GameMap.position = {0.0, 0.0};
     float speed = 4.0;
 
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knightIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knightRunning = LoadTexture("characters/knight_run_spritesheet.png");
+    Texture2D knight{knightIdle};
     Vector2 knightPosition = {
         (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
         (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
@@ -32,11 +34,14 @@ int main() {
     int frame{};
     const int maxFrame{6};
     const float updateTime{1.0/12.0};
+    bool isKnightMoving{false};
 
     while (!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        Texture2D activeSpritesheet{};
+        isKnightMoving ? activeSpritesheet = knightRunning : activeSpritesheet = knight;
 
         // movement of the character is achieved by
         // moving the game map texture, so that the character
@@ -47,8 +52,11 @@ int main() {
         if (IsKeyDown(KEY_W)) direction.y -= 1.0;
         if (IsKeyDown(KEY_S)) direction.y += 1.0;
         if (Vector2Length(direction) != 0.0) {
+            isKnightMoving = true;
             GameMap.position = Vector2Subtract(GameMap.position, Vector2Scale(Vector2Normalize(direction), speed));
             direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
+        } else {
+            isKnightMoving = false;
         }
 
         DrawTextureEx(GameMap.texture, GameMap.position, 0, 4, RAYWHITE);
@@ -62,7 +70,7 @@ int main() {
 
         Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
         Rectangle destination{knightPosition.x, knightPosition.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-        DrawTexturePro(knight, source, destination, Vector2{}, 0.0f, WHITE);
+        DrawTexturePro(activeSpritesheet, source, destination, Vector2{}, 0.0f, WHITE);
 
         EndDrawing();
     }
